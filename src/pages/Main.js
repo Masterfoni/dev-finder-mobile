2fa7491d24ec7f6e9f5e8f8b570c7f25a2708cc5
Masterfoni <joaovitor8891879@gmail.com>
2020-01-22 15:32:19 -0300

    Set up map (main) screen

---
+++ src/pages/Main.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+    
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords: { latitude, longitude } } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                });
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return <MapView initialRegion={currentRegion} style={styles.map} />
+};
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+});
+
+export default Main;
