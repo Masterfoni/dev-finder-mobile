8ae51a054a1d09022b0b8a77e7177258fea89a62
Masterfoni <joaovitor8891879@gmail.com>
2020-01-28 22:54:48 -0300

    [WIP] Add connection via socketio

---
+++ src/services/socket.js
@@ -0,0 +1,27 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://192.168.1.5:3333', {
+    autoConnect: false,
+});
+
+
+function connect(latitude, longitude, stack) {
+    socket.io.opts.query = {
+        latitude,
+        longitude,
+        stack,
+    };
+
+    socket.connect();
+}
+
+function disconnect() {
+    if (socket.connected) {
+        socket.disconnect;
+    }
+}
+
+export {
+    connect,
+    disconnect,
+};
